c5a318c71727571984ec97af05b3066c70045d69
Adam Lamagna <adamjames1920@gmail.com>
2014-02-07 11:50:09 -0500

    added database

--- cms/db_connect.php
+++ cms/db_connect.php
@@ -1,4 +1,10 @@
 <?php 
-$db = mysql_connect("localhost", "root", "Music2981") or die("Could not connect to databse.");
-mysql_select_db("julia", $db);
+if ("local.juliaanncampbell.com" === $_SERVER["SERVER_NAME"]) {
+	$db = mysql_connect("localhost", "root", "Music2981") or die("Could not connect to databse.");
+	mysql_select_db("julia", $db);
+} else {
+	$db = mysql_connect("db514361520.db.1and1.com", "dbo514361520" "jul!@camp") or die("Could not connect to database");
+	mysql_connect("db514361520", $db);
+}
+
 ?>
