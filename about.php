1ae886a3d0a112497f6295ff3fa2fb23df297c2c
Adam Lamagna <adamjames1920@gmail.com>
2014-02-07 10:50:38 -0500

    changed login

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php 
+//open connection to the database
+include ("functions.php");
+include("./cms/db_connect.php");
+
+//select 5 most recent posts
+$myposts = [];
+$sql = "SELECT post_id, title, post, DATE_FORMAT(postdate, '%e %b %Y at %H:%i') AS dateattime FROM posts ORDER BY postdate DESC, post_id DESC LIMIT 5";
+$result = mysql_query($sql);
+while($row = mysql_fetch_assoc($result)) {
+	$myposts[] = $row;
+}
+?> 
+
+
+<!doctype>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Julia Ann Campbell</title>
+		
+		<link href='http://fonts.googleapis.com/css?family=Bilbo+Swash+Caps|Felipa|Swanky+and+Moo+Moo|Just+Me+Again+Down+Here' rel='stylesheet' type='text/css'>
+		<script src="https://code.jquery.com/jquery.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="../dist/js/bootstrap.min.js"></script>
+		<link href="../dist/css/bootstrap.css" rel="stylesheet">
+		<link href="../css/font-awesome.min.css" rel="stylesheet">
+		<style type="text/css"> @import url(../css/julia.css); </style>
+
+	</head>
+	<body>
+		<div class="container">
+			
+			<?php include("header.php"); ?>
+			
+			
+			<!--this is the main part of the page-->
+			<div class="row">
+				<div class="col-sm-6 col-md-6">
+					<h3>About Julia</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
+					</p>
+				</div><!--end col-6-->
+				<div class="col-sm-6 col-md-6">
+					<img id="juliaFace" alt="Julia smiling" src="../img/julia1-1.jpg">
+				</div>
+			</div><!--end row-->
+			<?php include("footer.php"); ?>
+		</div><!--end container-->
+		<script src="script.js"></script>
+	</body>
+</html>
